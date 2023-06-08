ab40eaed3535f9dc7f4adc25c1bcea970a5d9b42
nyiyui <+@nyiyui.ca>
2023-06-08 02:37:15 -0400

    do some stuf

--- tej3traffic.ino
+++ tej3traffic.ino
@@ -5,58 +5,88 @@
 #define LED_PIN 11
 #define LDR_PIN A0
 
-struct upd {
-  bool on;
-  int pin;
-  int time;
+struct callback_request {
+  unsigned long wakeBy;
 };
 
-// === Traffic Light Pins
-// R Y G
-// 2 3 4 not dead end
-// 5 6 7 dead end
-// 8   9 (pedestrian)
-#define SCHEDULE_END 12000
-struct upd updates[] {
-  {false, 5, 0},
-  {true,  7, 0},
-  {true,  8, 0}, // people
-  {false, 9, 0}, // people
+#define flag_high(field, flag) (((field & flag) == flag) ? HIGH : LOW)
 
-  {false, 7, 2000},
-  {true,  6, 2000},
+#define LIGHT_R 0x1
+#define LIGHT_Y 0x2
+#define LIGHT_G 0x4
 
-  {false, 6, 4000},
-  {true,  5, 4000},
-
-  {false, 8, 6000}, // people (add a grace period)
-  {true,  9, 6000}, // people
-  {false, 2, 6000},
-  {true,  4, 6000},
-
-  {false, 4, 8000},
-  {true,  3, 8000},
-  {true,  8, 8000}, // people
-  {false, 9, 8000}, // people
-
-  {false, 3, 10000},
-  {true,  2, 10000},
+struct situation {
+  int through;
+  int dead_end;
+  int pedestrian;
 };
 
-#define interval 100
+void reconcile(struct situation now) {
+  static struct situation prev = {0};
+#  define flag_reconcile_debug(field, flag, pin) do { Serial.print(# field); Serial.print(# flag); Serial.print(now.field); Serial.print(","); Serial.print(flag); Serial.print(","); Serial.print((now.field & flag) == flag); Serial.print(" "); Serial.println(flag_high(now. field, flag)); digitalWrite(pin, flag_high(now. field, flag)); } while (0)
+#  define flag_reconcile(field, flag, pin) do { digitalWrite(pin, flag_high(now. field, flag)); } while (0)
+  // === Traffic Light Pins
+  // R Y G
+  // 2 3 4 not dead end
+  // 5 6 7 dead end
+  // 8   9 (pedestrian)
+  flag_reconcile(through,    LIGHT_R, 2);
+  flag_reconcile(through,    LIGHT_Y, 3);
+  flag_reconcile(through,    LIGHT_G, 4);
+  flag_reconcile(dead_end,   LIGHT_R, 5);
+  flag_reconcile(dead_end,   LIGHT_Y, 6);
+  flag_reconcile(dead_end,   LIGHT_G, 7);
+  flag_reconcile(pedestrian, LIGHT_R, 8);
+  flag_reconcile(pedestrian, LIGHT_G, 9);
+  prev = now;
+}
 
-// get new state for button press?
-int getButtonVal(int time) {
-  if (time <= 4000) return 3;
-  if (time <= 5500) return 2;
-  return 1;
+// Handles changes to signal lights.
+// Called when a) program starts OR b) callback_request.wakeBy is in the current or the past OR c) button pressed.
+struct callback_request step(bool button_pressed) {
+  static int state = 0;
+  static bool fast = false; // schedule after button is presed
+  unsigned long now = millis();
+  unsigned long wakeOffset = 2000;
+  if (button_pressed) fast = true;
+  switch (state) {
+  case 0:
+    reconcile({ LIGHT_R, LIGHT_G, LIGHT_R });
+    if (fast) wakeOffset = 3000;
+    break;
+  case 1:
+    if (fast && !button_pressed)
+      fast = false;
+    reconcile({ LIGHT_R, LIGHT_Y, LIGHT_R });
+    if (fast) wakeOffset = 1000;
+    break;
+  case 2:
+    reconcile({ LIGHT_R, LIGHT_R, LIGHT_R });
+    if (fast) wakeOffset = 1000;
+    break;
+  case 3:
+    reconcile({ LIGHT_G, LIGHT_R, LIGHT_G });
+    if (fast) wakeOffset = 3000;
+    break;
+  case 4:
+    if (fast && !button_pressed)
+      fast = false;
+    reconcile({ LIGHT_Y, LIGHT_R, LIGHT_R });
+    if (fast) wakeOffset = 1000;
+    break;
+  case 5:
+    reconcile({ LIGHT_R, LIGHT_R, LIGHT_R });
+    if (fast) wakeOffset = 1000;
+    break;
+  }
+  state ++;
+  struct callback_request ret = { now + wakeOffset };
+  return ret;
 }
 
 Servo servo;
-bool button;
-int curr; // current state?
-int prev, now, accel;
-int servoTime;
+unsigned long callStep = 0; // call step when millis() >= callStep
+int servoTime = 0;
 
 void setup()
 {
@@ -67,61 +97,38 @@ void setup()
   // === Buttons
   pinMode(BUTTON_PIN, INPUT);
   // === Traffic Lights
-  for (int i = 0; i < sizeof(updates)/sizeof(upd); i++)
-	pinMode(updates[i].pin, OUTPUT);
+  for (int pin = 2; pin <= 9; pin ++)
+    pinMode(pin, OUTPUT);
+  // === Servo
   servo.attach(SERVO_PIN);
-  
-  curr = -1; // idk
 }
 
 void loop()
 {
+  if (millis() >= callStep) {
+    bool button_pressed = digitalRead(BUTTON_PIN);
+    struct callback_request cr = step(button_pressed);
+    callStep = cr.wakeBy;
+    Serial.print("called ");
+    Serial.println(callStep);
+  }
+
   // === Buttons et al
-  now = (millis() + accel) % SCHEDULE_END;
   if (digitalRead(BUTTON_PIN)) {
     Serial.println("button pressed");
-    curr = getButtonVal(now % 7000);
   }
   
-  static unsigned long wait_until = millis();
-  if (millis() + accel > wait_until) {
-    wait_until = millis() + interval;
-    if (curr < 3 && curr > 0) accel += interval*2 /3;
-    else if (curr == 0) accel -= interval/3;
-  }
-  if (curr > 0) curr = getButtonVal(now % 7000);
-  if (curr == 0 && getButtonVal(now % 7000) == 3) curr = -1;
-
-  // === Traffic Lights
-  for (int i = 0; i < sizeof(updates)/sizeof(upd); i++) {
-    if (prev > now) { // handle modulo
-      prev = -1;Serial.println((servoTime % 360));
-  Serial.println(curr);
-
-    }
-    if (prev <= updates[i].time && updates[i].time <= now ) {
-      // Serial.print("pin ");
-      // Serial.print(updates[i].pin);
-      // Serial.print(" on ");
-      // Serial.print(updates[i].on);
-      // Serial.println();
-      digitalWrite(updates[i].pin, updates[i].on ? HIGH : LOW);
-    }
-  }
-  
+  // === Servo
   int adj = (servoTime % 360) >= 180 ? -1 : 1;
   servo.write(((servoTime % 360)/180)*180 + ((servoTime++) % 180) * adj);
-  // Serial.println((servoTime % 360));
-  // Serial.println(curr);
   
-  delay(1);
-  // to make sure prev < now (not prev == now!)
-  // NOTE: above does not handle SCHEDULE_END -> 0
-  prev = now;
+  // delay(1);
+  // // NOTE: to make sure cyclePrev < cycleNow (not cyclePrev == cycleNow!)
+  // //       (note that above does not handle SCHEDULE_END -> 0)
   
   // === Streetlight
   int val = analogRead(LDR_PIN);
   if (val > 120) digitalWrite(LED_PIN, HIGH);
   else digitalWrite(LED_PIN, LOW);
-  Serial.println(val);
 }
+
